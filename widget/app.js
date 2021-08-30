ea5ff87817b792beb2404d7ba2694799e181b434
alikaakati <kaakatiali@gmail.com>
2021-08-30 20:16:08 +0300

    removed unnecessary comments

--- widget/app.js
+++ widget/app.js
@@ -29,7 +29,7 @@
                 );
                 }
               }
-                // right here
+              
             } else {
               WidgetHome.data = {
                 content: {}
